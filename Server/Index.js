a832ea871028653fc253db2075ab30e766734096
Pavendra Singh Rajput <127604065+Pavendra21@users.noreply.github.com>
2024-08-31 19:23:15 +0530

    Update Index.js
    added frontend url

--- Server/Index.js
+++ Server/Index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: 'https://recipe-xers.vercel.app/', // Replace with your frontend URL
     credentials: true, // This is the key part
 }));
 
