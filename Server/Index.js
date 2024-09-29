fa6110d7982745757f5fb745e5f55030c3809a26
Pavendra Singh Rajput <127604065+Pavendra21@users.noreply.github.com>
2024-09-29 13:30:11 +0530

    Update Index.js

--- Server/Index.js
+++ Server/Index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://recipe-chi-seven.vercel.app, // Remove trailing slash
+  origin: 'https://recipe-chi-seven.vercel.app', // Remove trailing slash
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // Optional: add headers you expect
   credentials: true, // This is the key part
